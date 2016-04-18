6998fa6f3a0551e5ed98e190e6193225cc6ebfc7
Abhishek SP <abhisp@thoughtworks.com>
2016-04-18 23:07:40 +0530

    <Abhishek> [Medium] Initial Setup

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.json({message: 'hooray! welcome to our api!'});
+});
+app.use('/api', router);
+app.listen(port);
+console.log('Listening on :' + port);
