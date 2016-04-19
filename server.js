fbe803e54c7f29ea79c29da6df122b487348ad46
Abhishek SP <abhisp@thoughtworks.com>
2016-04-20 00:03:51 +0530

    <Abhishek> [Medium] Added Medium Parse logic and changed structure to follow heroku deploy standards

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
+var request = require('request');
+var cheerio = require('cheerio');
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.json({message: 'hooray! welcome to our api!'});
+var mediumQueryLink = 'https://medium.com/search?q=';
+router.get('/:query', function (req, res) {
+    var query = mediumQueryLink + req.params.query;
+    request(query, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            var posts = [];
+            $('div.postItem').each(function(i, element){
+                var post = {
+                    metadata: {
+                        authorName: $(element).find('div.postMetaInline-feedSummary').find('a').first().text(),
+                        authorProfileLink: $(element).find('div.postMetaInline-feedSummary').find('a').first().attr('href'),
+                        authorProfileImage: $(element).find('div.postMetaInline-avatar').find('img').attr('src'),
+                        publishedDate: $(element).find('div.postMetaInline-feedSummary').find('a').last().text(),
+                        readingTime: $(element).find('div.postMetaInline-feedSummary').find('span.readingTime').text()
+                    },
+                    title: $(element).find('div.section-inner').children().not('figure').first().text(),
+                    subTitle: $(element).find('div.section-inner').children().not('figure').last().text()
+                };
+                posts.push(post);
+            });
+            res.json(
+                {
+                    success: posts,
+                    error: false
+                }
+            );
+        }
+        else {
+            res.json(
+                {
+                    success: null,
+                    error: true
+                }
+            );
+        }
+    });
 });
-app.use('/api', router);
+
+app.use('/medium/posts', router);
 app.listen(port);
 console.log('Listening on :' + port);
