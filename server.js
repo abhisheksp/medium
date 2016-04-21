b44eeee3c750e49027aa2fbb0a39fa1b3666f37a
Abhishek SP <abhisp@thoughtworks.com>
2016-04-21 22:07:01 +0530

    <Abhishek> [Medium] Changed app settings to follow openshift deploy standards

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
-
 var posts = require('./posts.js')
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.use('/medium/posts', posts);
-app.listen(port);
-console.log('Listening on :' + port);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app;
